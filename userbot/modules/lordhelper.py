41c3d35f483a7d6312bab311f5679d80ba1875bd
Boncel-Cell <53797562+Boncel-Cell@users.noreply.github.com>
2021-06-24 05:27:32 +0800

    Update lordhelper.py

--- userbot/modules/lordhelper.py
+++ userbot/modules/lordhelper.py
@@ -11,10 +11,10 @@ DEFAULTUSER = str(ALIVE_NAME) if ALIVE_NAME else uname().node
 @register(outgoing=True, pattern="^.lhelp$")
 async def usit(e):
     await e.edit(
-        f"**Hai Lord {DEFAULTUSER} Kalau Anda Tidak Tau Perintah Untuk Memerintah Ku Ketik** `.help` Atau Bisa Minta Bantuan Ke:\n"
-        "\n[Telegram](t.me/liualvinas)"
-        "\n[Repo](https://github.com/zora24/Lord-Userbot)"
-        "\n[Instagram](Instagram.com/liualvinas_)")
+        f"**Hai Tot {DEFAULTUSER} Kalau Anda Tidak Tau Perintah Untuk Memerintah Ku Ketik** `.kontol` Atau Bisa Minta Bantuan Ke:\n"
+        "\n[Telegram](t.me/dims_whydi)"
+        "\n[Repo](https://XNXX.com)"
+        "\n[Instagram](Instagram.com/dimswhydi_)")
 
 
 @register(outgoing=True, pattern="^.vars$")
