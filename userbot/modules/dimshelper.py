d44fb50679459530abc6869b3e3d51e32ede64b0
Boncel-Cell <53797562+Boncel-Cell@users.noreply.github.com>
2021-06-25 05:38:55 +0800

    Update dimshelper.py

--- userbot/modules/dimshelper.py
+++ userbot/modules/dimshelper.py
@@ -8,12 +8,12 @@ DEFAULTUSER = str(ALIVE_NAME) if ALIVE_NAME else uname().node
 # ============================================
 
 
-@register(outgoing=True, pattern="^.lhelp$")
+@register(outgoing=True, pattern="^.dhelp$")
 async def usit(e):
     await e.edit(
-        f"**Hai Anak Kntll {DEFAULTUSER} Kalau Anda Tidak Tau Perintah Untuk Memerintah Ku Ketik** `.help` Atau Bisa Minta Bantuan Ke:\n"
+        f"**Hai Babu {DEFAULTUSER} Kalau Anda Tidak Tau Perintah Untuk Memerintah Ku Ketik** `.help` Atau Bisa Minta Bantuan Ke:\n"
         "\n[Telegram](t.me/dims_whydi)"
-        "\n[Repo](https://github.com/Boncel-Cell/kontoll)"
+        "\n[Repo](https://github.com/Boncel-Cell/Dims-Userbot)"
         "\n[Instagram](Instagram.com/Dimswhydi_)")
 
 
@@ -21,13 +21,13 @@ async def usit(e):
 async def var(m):
     await m.edit(
         f"**Disini Daftar Vars Dari {DEFAULTUSER}:**\n"
-        "\n[DAFTAR VARS](https://raw.githubusercontent.com/Zora24/Lord-Userbot/Lord-Userbot/varshelper.txt)")
+        "\n[DAFTAR VARS](https://raw.githubusercontent.com/Boncel-Cell/Dims-Userbot/Dims-Userbot/varshelper.txt)")
 
 
 CMD_HELP.update({
     "dimshelper":
-    "`.dimshelp`\
+    "`.dhelp`\
 \nPenjelasan: Bantuan Untuk Dims-Userbot.\
-\n`.dimsvar`\
+\n`.var`\
 \nPenjelasan: Untuk Melihat Beberapa Daftar Vars."
 })
