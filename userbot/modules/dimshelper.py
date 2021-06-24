138dd5a7ea00d35ba7e39940c5ae766fb867630d
Boncel-Cell <53797562+Boncel-Cell@users.noreply.github.com>
2021-06-24 23:05:52 +0800

    Update and rename lordhelper.py to dimshelper.py

--- userbot/modules/dimshelper.py
+++ userbot/modules/dimshelper.py
@@ -11,10 +11,10 @@ DEFAULTUSER = str(ALIVE_NAME) if ALIVE_NAME else uname().node
 @register(outgoing=True, pattern="^.lhelp$")
 async def usit(e):
     await e.edit(
-        f"**Hai Lord {DEFAULTUSER} Kalau Anda Tidak Tau Perintah Untuk Memerintah Ku Ketik** `.help` Atau Bisa Minta Bantuan Ke:\n"
-        "\n[Telegram](t.me/liualvinas)"
-        "\n[Repo](https://github.com/zora24/Lord-Userbot)"
-        "\n[Instagram](Instagram.com/liualvinas_)")
+        f"**Hai Anak Kntll {DEFAULTUSER} Kalau Anda Tidak Tau Perintah Untuk Memerintah Ku Ketik** `.help` Atau Bisa Minta Bantuan Ke:\n"
+        "\n[Telegram](t.me/dims_whydi)"
+        "\n[Repo](https://github.com/Boncel-Cell/kontoll)"
+        "\n[Instagram](Instagram.com/Dimswhydi_)")
 
 
 @register(outgoing=True, pattern="^.vars$")
@@ -25,9 +25,9 @@ async def var(m):
 
 
 CMD_HELP.update({
-    "lordhelper":
-    "`.lordhelp`\
-\nPenjelasan: Bantuan Untuk Lord-Userbot.\
-\n`.lordvar`\
+    "dimshelper":
+    "`.dimshelp`\
+\nPenjelasan: Bantuan Untuk Dims-Userbot.\
+\n`.dimsvar`\
 \nPenjelasan: Untuk Melihat Beberapa Daftar Vars."
 })
